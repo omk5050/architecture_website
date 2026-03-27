c3b54164c066c3bb706ba1f775d7b3582f2e6f36
Om Kadam <kadamom313@gmail.com>
2026-03-27 13:56:29 +0530

    feat: Replaced nodemailer with resend api

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,18 +1,19 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  try {
+    const response = await resend.emails.send({
+      from: process.env.EMAIL_FROM,
+      to: [to],
+      subject: subject,
+      html: html,
+    });
 
-  await transporter.sendMail({
-    from: `"Archidex" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html,
-  });
+    return response;
+  } catch (error) {
+    console.error("Email error:", error);
+    throw new Error("Email sending failed");
+  }
 };
